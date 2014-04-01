fb57240e6938d833bb46e29b22ea47dea58a8de2
Enrico Bruschini <enrico.bruschini@gmail.com>
2014-03-31 19:37:44 -0700

    dropbox app keys

--- js/dropbox-key-example.js
+++
@@ -1,4 +0,0 @@
-// Copy this file to dropbox-key.js and replace XXXX with your
-// Dropbox API key
-var DROPBOX_APP_KEY = 'XXXX',
-    dropbox = new Dropbox.Client({key: DROPBOX_APP_KEY});
---
+++ js/dropbox-key.js
@@ -0,0 +1,4 @@
+// Copy this file to dropbox-key.js and replace XXXX with your
+// Dropbox API key
+var DROPBOX_APP_KEY = 'bd9wi7q5rooz5gp',
+    dropbox = new Dropbox.Client({key: bd9wi7q5rooz5gp});
