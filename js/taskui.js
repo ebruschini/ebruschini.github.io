4aa61ba6797590453d8bcad9e659d41bd2f5fad5
Enrico Bruschini <enrico.bruschini@gmail.com>
2014-04-02 19:58:14 -0700

    event not e.

--- js/taskui.js
+++ js/taskui.js
@@ -326,7 +326,7 @@ $( document ).on( "mobileinit", function () {
   console.log('adding event handler to dropbox-login');
 	$("#dropbox-login").on("click", function (e) {
     console.log('GO GO GO DROPBOX!!!');
-		e.preventDefault();
+		event.preventDefault();
 		// This will redirect the browser to OAuth login.
 		dropbox.authenticate();
   });
